a2307cf98d3eca2f8ecb5e7c3050e7afe1534441
galiLikeLike <galiLikeLike@qq.com>
2024-09-13 21:18:32 +0800

    第一版,跑通换页功能

---
+++ src/main/java/com/sky/expand/BeanListner.java
@@ -0,0 +1,29 @@
+package com.sky.expand;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanListner implements BeanPostProcessor {
+
+    private Logger logger = LoggerFactory.getLogger(BeanListner.class);
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean,String beanName) throws BeansException {
+		if(beanName.contains("book")) {
+			logger.info(String.format("%s 即将初始化", beanName));
+		}
+		return bean;
+	}
+	
+	@Override
+	public Object postProcessAfterInitialization(Object bean,String beanName) throws BeansException {
+		if(beanName.contains("book")) {
+			logger.info(String.format("%s 即将初始化",beanName));
+		}
+		return bean;
+    }
+}
